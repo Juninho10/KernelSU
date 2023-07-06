1ccf1585f974f223f2ed806b94fb01b684c84196
Juninho10 <eliascordeiro835@gmail.com>
2023-07-06 16:59:31 -0300

    build MODULE.TARGET.EXECUTABLES.su from source
    Fix compilation error: build/make/core/base_rules.mk:325: error: system/extras/su: MODULE.TARGET.EXECUTABLES.su already defined by kernel/motorola/trinket/KernelSU/userspace/su/jni.

--- userspace/su/jni/Android.mk
+++ userspace/su/jni/Android.mk
@@ -1,6 +1,6 @@
-LOCAL_PATH := $(call my-dir)
+#LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := su
-LOCAL_SRC_FILES := su.c
-include $(BUILD_EXECUTABLE)
+#include $(CLEAR_VARS)
+#LOCAL_MODULE := su
+#LOCAL_SRC_FILES := su.c
+#include $(BUILD_EXECUTABLE)
